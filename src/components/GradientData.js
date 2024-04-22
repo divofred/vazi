ff0c2e0fd23cf97b47003e19d4bd94b9fc98f41a
Fems465 <feranmi465@gmail.com>
2024-04-22 09:59:33 +0100

    Update GradientData.js

--- src/components/GradientData.js
+++ src/components/GradientData.js
@@ -4,28 +4,35 @@ const gcards = [
   {
     gradientFrom: "#FFB100",
     gradientTo: "rgba(255,179,0,0)",
-    imageUrl: "/home/sc-01.png",
+    imageUrl: "/home/sc-01.jpg",
     // other data properties for the first set
   },
 
   {
     gradientFrom: "#1193A9",
     gradientTo: "#ffb30000",
-    imageUrl: "/home/sc-02.png",
+    imageUrl: "/home/sc-02.jpg",
     // other data properties for the first set
   },
 
   {
     gradientFrom: "#05373C",
     gradientTo: "rgba(255,179,0,0)",
-    imageUrl: "/home/sc-02.png",
+    imageUrl: "/home/sc-03.jpg",
     // other data properties for the first set
   },
 
   {
     gradientFrom: "#FFB100",
     gradientTo: "rgba(255,179,0,0)",
-    imageUrl: "/home/sc-02.png",
+    imageUrl: "/home/sc-04.jpg",
+    // other data properties for the first set
+  },
+
+    {
+    gradientFrom: "#FFB100",
+    gradientTo: "rgba(255,179,0,0)",
+    imageUrl: "/home/sc-05.jpg",
     // other data properties for the first set
   },
 
